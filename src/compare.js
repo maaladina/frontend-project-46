302a38aa3ee8642d495430b6eabbce89157a59b2
maria <maaladina@yandex.ru>
2023-04-12 20:47:19 +0300

    update compare.js

--- src/compare.js
+++ src/compare.js
@@ -3,15 +3,16 @@ import _ from 'lodash';
 const genStr = (key, value, sign = ' ') => `  ${sign} ${key}: ${value}\n`;
 
 const differenceForKey = (key, file1, file2) => {
-  if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
-    if (file1[key] === file2[key]) {
-      return genStr(key, file1[key]);
-    }
-    return genStr(key, file1[key], '-') + genStr(key, file2[key], '+');
-  } if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
+  if (!Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
+    return genStr(key, file2[key], '+');
+  }
+  if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
     return genStr(key, file1[key], '-');
   }
-  return genStr(key, file2[key], '+');
+  if (file1[key] === file2[key]) {
+    return genStr(key, file1[key]);
+  }
+  return genStr(key, file1[key], '-') + genStr(key, file2[key], '+');
 };
 
 const compare = (file1, file2) => {
