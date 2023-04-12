4c6942f97abdfe65e7ffb6081b480e0b12aaea27
maria <maaladina@yandex.ru>
2023-04-12 20:36:31 +0300

    update compare.js

--- src/compare.js
+++ src/compare.js
@@ -2,20 +2,22 @@ import _ from 'lodash';
 
 const genStr = (key, value, sign = ' ') => `  ${sign} ${key}: ${value}\n`;
 
+const differenceForKey = (key, file1, file2) => {
+  if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
+    if (file1[key] === file2[key]) {
+      return genStr(key, file1[key]);
+    }
+    return `${genStr(key, file1[key], '-')}${genStr(key, file2[key], '+')}`;
+  } if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
+    return genStr(key, file1[key], '-');
+  }
+  return genStr(key, file2[key], '+');
+};
+
 const compare = (file1, file2) => {
   const file = { ...file1, ...file2 };
   const sortedKeys = _.sortBy(Object.keys(file));
-  const result = sortedKeys.reduce((acc, key) => {
-    if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
-      if (file1[key] === file2[key]) {
-        return `${acc}${genStr(key, file1[key])}`;
-      }
-      return `${acc}${genStr(key, file1[key], '-')}${genStr(key, file2[key], '+')}`;
-    } if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
-      return `${acc}${genStr(key, file1[key], '-')}`;
-    }
-    return `${acc}${genStr(key, file2[key], '+')}`;
-  }, '');
+  const result = sortedKeys.reduce((acc, key) => acc + differenceForKey(key, file1, file2), '');
   return `{\n${result}}`;
 };
 
