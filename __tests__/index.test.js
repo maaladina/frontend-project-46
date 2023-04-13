af87afdfc19dce0b8a6a76de5c0525088f72495e
maria <maaladina@yandex.ru>
2023-04-13 12:51:14 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,9 +9,17 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff', () => {
+test('genDiff .json', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
+  const expResult = readFile('expected_file.json');
+  const result = genDiff(file1, file2);
+  expect(result).toEqual(expResult);
+});
+
+test('genDiff .yml', () => {
+  const file1 = getFixturePath('file1.yml');
+  const file2 = getFixturePath('file2.yml');
   const expResult = readFile('expected_file.json');
   const result = genDiff(file1, file2);
   expect(result).toEqual(expResult);
