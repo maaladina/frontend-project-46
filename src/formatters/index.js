c97d8fe4f868d96f9fe9ef370169b1fcaf02f213
maria <maaladina@yandex.ru>
2023-04-30 20:36:13 +0300

    changed the name of the getFormattedResult function to format

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,14 +2,14 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const getFormattedResult = (diff, format) => {
-  if (format === 'plain') {
+const format = (diff, formatName) => {
+  if (formatName === 'plain') {
     return plain(diff);
   }
-  if (format === 'json') {
+  if (formatName === 'json') {
     return json(diff);
   }
   return stylish(diff);
 };
 
-export default getFormattedResult;
+export default format;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import compare from './compare.js';
-import getFormattedResult from './formatters/index.js';
+import format from './formatters/index.js';
 import parse from './parser.js';
 
 const getPath = (file) => path.resolve(process.cwd(), file);
@@ -14,7 +14,7 @@ const genDiff = (file1Path, file2Path, formatName = 'stylish') => {
   const parsedData1 = parse(data1, getType(file1Path));
   const parsedData2 = parse(data2, getType(file2Path));
   const diff = compare(parsedData1, parsedData2);
-  const result = getFormattedResult(diff, formatName);
+  const result = format(diff, formatName);
   return result;
 };
 
