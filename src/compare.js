64527999ea7e0b9143894867a3322eccdb3e6a2c
maria <maaladina@yandex.ru>
2023-04-30 20:59:55 +0300

    update compare

--- src/compare.js
+++ src/compare.js
@@ -1,39 +1,24 @@
 import _ from 'lodash';
 
-const getType = (data1, data2, key) => {
-  if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
-    return 'deleted';
-  }
-  if (!Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
-    return 'added';
-  }
-  if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-    return 'check';
-  }
-  if (data1[key] !== data2[key]) {
-    return 'changed';
-  }
-  return 'unchanged';
-};
-
 const compare = (data1, data2) => {
-  const obj = { ...data1, ...data2 };
-  const sortedKeys = _.sortBy(Object.keys(obj));
+  const data = { ...data1, ...data2 };
+  const sortedKeys = _.sortBy(Object.keys(data));
   const diffObj = sortedKeys.flatMap((key) => {
-    switch (getType(data1, data2, key)) {
-      case 'check':
-        return { key, value: compare(data1[key], data2[key]), type: 'check' };
-      case 'deleted':
-        return { key, value: data1[key], type: 'deleted' };
-      case 'added':
-        return { key, value: data2[key], type: 'added' };
-      case 'changed':
-        return {
-          key, value1: data1[key], value2: data2[key], type: 'changed',
-        };
-      default:
-        return { key, value: data1[key], type: 'unchanged' };
+    if (!Object.hasOwn(data2, key)) {
+      return { key, value: data1[key], type: 'deleted' };
     }
+    if (!Object.hasOwn(data1, key)) {
+      return { key, value: data2[key], type: 'added' };
+    }
+    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      return { key, value: compare(data1[key], data2[key]), type: 'check' };
+    }
+    if (data1[key] !== data2[key]) {
+      return {
+        key, value1: data1[key], value2: data2[key], type: 'changed',
+      };
+    }
+    return { key, value: data1[key], type: 'unchanged' };
   });
   return diffObj;
 };
