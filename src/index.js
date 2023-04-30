7eda60301b5da2f5be1f8694390ff9b524ec844e
maria <maaladina@yandex.ru>
2023-04-30 20:30:46 +0300

    changed the name of the getFormat function to getType

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import parse from './parser.js';
 
 const getPath = (file) => path.resolve(process.cwd(), file);
 const readFile = (file) => fs.readFileSync(getPath(file));
-const getfFormat = (filename) => path.extname(filename).slice(1);
+const getType = (filename) => path.extname(filename).slice(1);
 
 const genDiff = (file1Path, file2Path, formatName = 'stylish') => {
   const data1 = readFile(file1Path);
   const data2 = readFile(file2Path);
-  const parsedData1 = parse(data1, getfFormat(file1Path));
-  const parsedData2 = parse(data2, getfFormat(file2Path));
+  const parsedData1 = parse(data1, getType(file1Path));
+  const parsedData2 = parse(data2, getType(file2Path));
   const diff = compare(parsedData1, parsedData2);
   const result = getFormattedResult(diff, formatName);
   return result;
